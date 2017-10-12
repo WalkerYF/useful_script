ce83b218aab052cef9ce35655a52909f12a35017
laptop <walkeryf@yeah.net>
2017-10-12 16:55:32 +0800

    modifly the sync.sh

--- sync.sh
+++ sync.sh
@@ -5,12 +5,18 @@ do
 	temp=`git pull`
 	echo "$a : $temp"
 	cd ..
+	# echo ${temp:0:4}
+	# echo ${temp:0:6}
+	# echo ${temp%:*}
 	if [ "$temp" == "Already up-to-date." ]; then
 		temp="Already up-to-date."
-	else
-		temp="Updated!"
+	elif [ "${temp:0:4}" == "error" ]; then
+		temp="Error!"
+	else 
+		temp="maybe sync success"
 	fi
-	k[index]="\t$a\t:$temp\t"
+	# k[index]="\t$a\t:$temp\t"
+	k[index]=`printf "%30s : %20s" "$a" "$temp"`
 	# echo -e "${k[$index]}"
 	index=$index+1
 done
