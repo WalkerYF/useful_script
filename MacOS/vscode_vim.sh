84c5ea4a3bb0b47a05975fb84d26cb43bff8d6c5
wwyf <w.y.ff@163.com>
2021-04-02 17:11:01 +0800

    update

---
+++ MacOS/vscode_vim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#https://www.jianshu.com/p/7aa5f6d5358e
+# https://marketplace.visualstudio.com/items?itemName=vscodevim.vim
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
+#defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
+
