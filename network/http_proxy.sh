1e8b87f16835a82e55e4e1a4b0b535e3642252bc
wwyf <w.y.ff@163.com>
2021-04-19 11:03:00 +0800

    update

--- network/http_proxy.sh
+++ network/http_proxy.sh
@@ -3,7 +3,7 @@
 # https://www.atjiang.com/pragmatic-tmux-scripting-customized-envs/
 name=http_proxy_nvm
 fq_port=1087
-ch_port=52342
+ch_port=52341
 remote_port=52341
 
 status="ch"
@@ -35,7 +35,9 @@ fi
 # ssh -D ${local_port} localhost
 # open an http proxy on my laptop (bug in macos)
 # ncat -l ${local_port} --proxy-type http
-
+# open a http proxy use proxy.py in python (pip install proxy.py)
+# https://github.com/abhinavsingh/proxy.py
+# /opt/anaconda3/bin/python -m 'proxy' --hostname '::' --port ${ch_port}"
 
 tmux new-session -s $name -n editor -d
 tmux split-window -v -t $name
@@ -43,7 +45,7 @@ tmux split-window -v -t $name
 #tmux send-keys -t $name:0.0 'cd tools' C-m
 #tmux send-keys -t $name:0.0 'cd v2ray-core' C-m
 #tmux send-keys -t $name:0.0 './v2ray -c http_proxy.json' C-m
-tmux send-keys -t $name:0.0 "ncat -l ${ch_port} --proxy-type http" C-m
+tmux send-keys -t $name:0.0 "/opt/anaconda3/bin/python -m 'proxy' --hostname '::' --port ${ch_port}" C-m
 tmux send-keys -t $name:0.1 "ssh -R ${remote_port}:localhost:${local_port} pm8" C-m
 tmux send-keys -t $name:0.2 "ssh -R ${remote_port}:localhost:${local_port} pm9" C-m
 #tmux split-window -v -t development
@@ -58,7 +60,6 @@ if [[ ${my_input} = "q" ]]
 then
     status="q"
 fi
-
 
 
 done
